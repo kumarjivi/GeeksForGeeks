c757037605c9fb4daf72170ef3b96a67a9fe36ec
Kumar Jivi <kumarjivi@yahoo.com>
2017-12-05 16:29:00 -0800

    Added Test Class

---
+++ GeeksForGeeks/src/stepByStep/sorting/SortingTest.java
@@ -0,0 +1,26 @@
+package stepByStep.sorting;
+
+import utilityMethods.ArrayUtils;
+
+public class SortingTest {
+
+    public static void main(String args[]) {
+        SelectionSort sort = new SelectionSort();
+        int[] arr = {5,6,7,1,2,6,4,1,2,4,12,32,4};
+        System.out.println("Selection Sort: ");
+        System.out.println("Before:");
+        ArrayUtils.printIntArray(arr);
+        sort.selectionSort(arr);
+        System.out.println("After:");
+        ArrayUtils.printIntArray(arr);
+
+        InsertionSort insertionSort = new InsertionSort();
+        int ins[] = {12,43,1,4,0,5,6,7};
+        System.out.println("Insertion Sort: ");
+        System.out.println("Before:");
+        ArrayUtils.printIntArray(ins);
+        insertionSort.insertionSort(ins);
+        System.out.println("After:");
+        ArrayUtils.printIntArray(ins);
+    }
+}
