ca80972dc3ead1915d337f48bb3c8c8a0c9c6938
Kumar Jivi <kumarjivi@yahoo.com>
2018-06-17 13:26:11 -0700

    With memo

--- GeeksForGeeks/src/dp/basic/LongestCommonSubsequence32.java
+++ GeeksForGeeks/src/dp/basic/LongestCommonSubsequence32.java
@@ -1,4 +1,7 @@
 package dp.basic;
+
+import java.util.Scanner;
+
 /**
  * Given two sequences, find the length of longest subsequence present in both of them.
  * A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
@@ -7,17 +10,39 @@ package dp.basic;
  * @author jivi
  *
  */
+/*
+2
+6 6
+ABCDGH
+AEDFHR
+3 2
+ABC
+AC
+ */
 public class LongestCommonSubsequence32 {
 
 	public static void main(String[] args) {
-		String one = "ABCDGH";
-		String two = "AEDFHR";
-		System.out.println(getLongestCommonSubseq(one, two));
+		Scanner sc = new Scanner(System.in);
+		int numCases = sc.nextInt();
+		for(int k=0; k<numCases; k++) {
+			sc.nextInt();
+			sc.nextInt();
+			String one = sc.next();
+			String two = sc.next();
+			
+			System.out.println("Length: "+getLongestCommonSubseq(one, two));
+		}		
 	}
-	
+	static int[][] memo;
 	public static int getLongestCommonSubseq(String str1, String str2) {
 		if(str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
 			return 0;
+		}
+		memo = new int[str1.length()][str2.length()];
+		for(int i=0; i<memo.length; i++) {
+			for(int j=0; j<memo[0].length; j++) {
+				memo[i][j] = -1;
+			}
 		}
 		return helper(str1.toCharArray(), 0, str2.toCharArray(), 0);
 	}
@@ -26,20 +51,27 @@ public class LongestCommonSubsequence32 {
 		if(i >= s.length || j >= t.length) {
 			return 0;
 		}
+		if(memo[i][j] != -1) {
+			return memo[i][j];
+		}
 		if(i == s.length-1 && j == t.length-1) {//if its the last character of both.
 			if(s[i] == t[j]) {
+				memo[i][j] = 1;
 				return 1;
 			} else {
+				memo[i][j] = 0;
 				return 0;
 			}
 		}
 		if(s[i] == t[j]) {
 			int ans = 1 + helper(s, i+1, t, j+1);
+			memo[i][j] = ans;
 			return ans;
 		} else {
 			int one = helper(s, i+1, t, j);
 			int two = helper(s, i, t, j+1);
-			return Math.max(one, two);
+			memo[i][j] = Math.max(one, two);
+			return memo[i][j];
 		}
 	}
 }
